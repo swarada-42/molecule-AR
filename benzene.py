3edb02ea4bf0dbea72daae94ceef905b17581bab
Swarada Ware <swarada42@gmail.com>
2025-12-26 13:17:23 +0530

    Add files via upload

---
+++ benzene.py
@@ -0,0 +1,23 @@
+import cv2
+import cv2.aruco as aruco
+
+# Choose dictionary
+aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
+
+# Marker ID (any number 0–49)
+marker_id = 7
+
+# Marker size (pixels)
+marker_size = 600
+
+# Generate marker
+marker_img = aruco.generateImageMarker(
+    aruco_dict,
+    marker_id,
+    marker_size
+)
+
+# Save marker
+cv2.imwrite("benzene.png", marker_img)
+
+print("✅ Marker created: benzene.png")
